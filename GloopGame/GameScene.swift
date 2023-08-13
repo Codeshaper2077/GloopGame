8bfb0b24c58440fd36ac3a880565393d3c9898c4
Ivan <ivan.Nikolaevich55@gmail.com>
2023-08-14 02:28:15 +0500

    update: update code

--- GloopGame/GameScene.swift
+++ GloopGame/GameScene.swift
@@ -9,5 +9,20 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-
+    override func didMove(to view: SKView) {
+        //set bg
+        let background = SKSpriteNode(imageNamed: "background_1")
+        background.anchorPoint = CGPoint(x: 0.0, y: 0.0)
+        background.position = CGPoint(x: 0.0, y: 0.0)
+        addChild(background)
+        
+        //set fg
+        let foreground = SKSpriteNode(imageNamed: "foreground_1")
+        foreground.anchorPoint = CGPoint(x: 0.0, y: 0.0)
+        foreground.position = CGPoint(x: 0.0, y: 0.0)
+        addChild(foreground)
+        
+        //...
+        
+    }
 }
--- GloopGame/GameViewController.swift
+++ GloopGame/GameViewController.swift
@@ -13,15 +13,27 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        
+        if let view = self.view as! SKView? {
+            //1336 × 1024
+            let scene = GameScene(size: CGSize(width: 1336, height: 1024))
+            print("width\(view.frame.width), height:\(view.frame.height)")
+            scene.scaleMode = .aspectFill
+            scene.backgroundColor = UIColor(red: 20/255, green: 120/255, blue: 200/255, alpha: 1.0)
+            
+            view.showsPhysics = false
+            view.showsFPS = true
+            view.showsNodeCount = true
+            view.ignoresSiblingOrder = false
+            
+            view.presentScene(scene)
+        }
+        
+        
     }
 
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
+        return .landscape
     }
 
     override var prefersStatusBarHidden: Bool {
